8e7d071bbbe19246024b40f06cf01da4a22d1c9b
zazannn <rizan@kodingnext.com>
2026-07-01 14:36:53 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -23,3 +23,17 @@ temp_map = bundle['temp_map']
 hr_map = bundle['hr_map']
 dur_map = bundle['dur_map']
 cc_map = bundle['cc_map']
+
+st.header("Patient Information")
+
+age = st.number_input(
+  "Age",
+  min_value=1,
+  max_value=120,
+  value=35
+)
+
+gender = st.selectbox(
+  "Gender",
+  ["Female", "Male"]
+)
