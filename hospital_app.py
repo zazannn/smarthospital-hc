c553868cde56f251fed7adce0d0090844c3fa414
zazannn <rizan@kodingnext.com>
2026-07-01 14:52:10 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -50,3 +50,19 @@ nausea_vomiting = st.checkbox("Nausea / Vomiting")
 dizziness = st.checkbox("Dizziness")
 skin_rash = st.checkbox("Skin Rash")
 
+st.header("Medical Information")
+
+chief_complaint = st.selectbox(
+  "Chief Complaint",
+  list(cc_map.keys())
+)
+
+duration = st.selectbox(
+  "Duration",
+  list(dur_map.keys())
+)
+
+temperature_level = st.selectbox(
+ "Temperature",
+  list(temp_map.keys())
+)
